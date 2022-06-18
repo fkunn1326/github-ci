4a8475c14ce4bc1499a2443a5325cbb7485f196d
Fkunn1326 <92153597+fkunn1326@users.noreply.github.com>
2022-06-18 21:21:17 +0900

    Update linux_configure.sh

--- scripts/linux_configure.sh
+++ scripts/linux_configure.sh
@@ -3,7 +3,7 @@ sudo apt update
 sudo apt install -y socat xfce4 xfce4-goodies xfonts-base xubuntu-icon-theme xubuntu-wallpapers gnome-icon-theme x11-apps x11-common x11-session-utils x11-utils x11-xserver-utils x11-xkb-utils dbus-user-session dbus-x11 gnome-system-monitor gnome-control-center libpam0g libxt6 libxext6
 
 # Second, install TurboVNC
-wget https://phoenixnap.dl.sourceforge.net/project/turbovnc/2.2.5/turbovnc_2.2.5_amd64.deb
+wget https://jaist.dl.sourceforge.net/project/virtualgl/3.0.1/virtualgl_3.0.1_amd64.deb
 sudo dpkg -i turbovnc_2.2.5_amd64.deb
 
 # Fourth, generate and copy passwd file and xstartup script
