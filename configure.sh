93066963e6d01271204828cf8f014e9d6d810ab3
Fkunn1326 <92153597+fkunn1326@users.noreply.github.com>
2022-06-18 10:39:20 +0900

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -27,4 +27,4 @@ sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resourc
 #start piping
 curl -L https://github.com/nwtgck/piping-server-pkg/releases/download/v1.12.3/piping-server-pkg-mac-x64.tar.gz | tar xzvf -
 #sudo xattr -d com.apple.quarantine ./piping-server-pkg-mac-x64/piping-server
-./piping-server-pkg-mac-x64/piping-server -s https://ppng.io server -p 5900 fkunn1326 fkunn1326
+./piping-server-pkg-mac-x64/piping-server -s https://ppng.io server -p 5900 fkunn1326 fkunn1327
