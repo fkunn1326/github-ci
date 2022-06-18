425c96cc89804c3c28bb34cd01d79853b114f8b9
Fkunn1326 <92153597+fkunn1326@users.noreply.github.com>
2022-06-18 10:43:52 +0900

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -29,4 +29,5 @@ sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resourc
 #sudo xattr -d com.apple.quarantine ./piping-server-pkg-mac-x64/piping-server
 #./piping-server-pkg-mac-x64/piping-server -s https://ppng.io server -p 5900 fkunn1326 fkunn1327
 
+brew install socat
 socat 'EXEC:curl -NsS https\://ppng.io/fkunn1326!!EXEC:curl -NsST - https\://ppng.io/fknn1327' TCP:127.0.0.1:5900
