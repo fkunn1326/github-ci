07c60812a3f58e8baa18f7219ae261a033c38c88
Fkunn1326 <92153597+fkunn1326@users.noreply.github.com>
2022-06-19 21:28:21 +0900

    Update Backup.ps1

--- scripts/Backup.ps1
+++ scripts/Backup.ps1
@@ -4,8 +4,8 @@ git config --global user.name "fkunn1326"
 $now = Get-Date -format "yyyy/MM/dd HH:mm"
 
 Set-Location "C:\Users\runneradmin\AppData\Local\Google\Chrome\User Data"
-echo +$now >> README.md
-7z.exe a backups.zip .
+echo $now >> README.md
+Compress-Archive -Path * -DestinationPath D:\a\github-ci\github-ci\backups\backup.zip
 
 Move-Item backups.zip D:\a\github-ci\github-ci\backups
 Set-Location D:\a\github-ci\github-ci\backups
