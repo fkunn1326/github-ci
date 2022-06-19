6df9eff0f43e74c93366cf30de6d7e60c0d710d5
Fkunn1326 <92153597+fkunn1326@users.noreply.github.com>
2022-06-19 16:37:04 +0900

    Update Backup.ps1

--- scripts/Backup.ps1
+++ scripts/Backup.ps1
@@ -4,10 +4,16 @@ git config --global user.name "fkunn1326"
 $now = Get-Date -format "yyyy/MM/dd HH:mm"
 
 Set-Location "C:\Users\runneradmin\AppData\Local\Google\Chrome\User Data"
-#echo +$now >> README.md
-#git add .
-#git commit -m $now
-#git branch -M main
-#git remote add origin https://$Args@github.com/fkunn1326/ghvnc-secret
-#git pull origin　main
-#git push -u origin main
+echo +$now >> README.md
+7z.exe a backups.zip .
+
+Move-Item backups.zip $GITHUB_WORKSPACE\backups
+Set-Location $GITHUB_WORKSPACE
+
+echo +$now >> README.md
+git add .
+git commit -m $now
+git branch -M main
+git remote add origin https://$Args@github.com/fkunn1326/ghvnc-secret
+git pull origin　main
+git push -u origin main
