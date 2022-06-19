b199d16874b6df92d4162a85d02bbb3cf9ad4ec5
Fkunn1326 <92153597+fkunn1326@users.noreply.github.com>
2022-06-19 15:29:10 +0900

    Update Backup.ps1

--- scripts/Backup.ps1
+++ scripts/Backup.ps1
@@ -4,7 +4,7 @@ git config --global user.name "fkunn1326"
 $now = Get-Date -format "yyyy/MM/dd HH:mm"
 
 Set-Location "C:\Users\runneradmin\AppData\Local\Google\Chrome\User Data"
-echo -e "# Backup\n${now}" >> README.md
+echo "# Backup\n${now}" >> README.md
 git init
 git add .
 git commit -m $now
