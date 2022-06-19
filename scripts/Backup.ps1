5c2d9e40db07ac77626f7a947ecd9bd65183f943
Fkunn1326 <92153597+fkunn1326@users.noreply.github.com>
2022-06-19 22:29:16 +0900

    Update Backup.ps1

--- scripts/Backup.ps1
+++ scripts/Backup.ps1
@@ -5,7 +5,7 @@ $now = Get-Date -format "yyyy/MM/dd HH:mm"
 
 Set-Location "C:\Users\runneradmin\AppData\Local\Google\Chrome\User Data"
 echo $now >> README.md
-Compress-Archive -Path * -DestinationPath D:\a\github-ci\github-ci\backups\backup.zip
+Compress-Archive -Path * -DestinationPath D:\a\github-ci\github-ci\backups\backup.zip -Update
 
 #Move-Item backups.zip D:\a\github-ci\github-ci\backups
 Set-Location D:\a\github-ci\github-ci\backups
