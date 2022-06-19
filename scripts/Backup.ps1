7bd17e6e2bb9b01196d30cd3b7523ea7b0f2e5c2
Fkunn1326 <92153597+fkunn1326@users.noreply.github.com>
2022-06-19 15:04:36 +0900

    Update Backup.ps1

--- scripts/Backup.ps1
+++ scripts/Backup.ps1
@@ -1,3 +1,6 @@
+git config --global user.email "2007.yfukuda@gmail.com"
+git config --global user.name "fkunn1326"
+
 $now = Get-Date -format "yyyy/MM/dd HH:mm"
 
 Set-Location "C:\Users\runneradmin\AppData\Local\Google\Chrome\User Data"
