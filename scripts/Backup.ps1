6548ed3042a27f33bd8c2eaaa5cbeac54e54b231
Fkunn1326 <92153597+fkunn1326@users.noreply.github.com>
2022-06-19 15:50:46 +0900

    Update Backup.ps1

--- scripts/Backup.ps1
+++ scripts/Backup.ps1
@@ -5,10 +5,10 @@ $now = Get-Date -format "yyyy/MM/dd HH:mm"
 
 Set-Location "C:\Users\runneradmin\AppData\Local\Google\Chrome\User Data"
 gh repo clone fkunn1326/ghvnc-secret .
-echo +$now >> README.md
-git add .
-git commit -m $now
-git branch -M main
-git remote add origin https://$Args@github.com/fkunn1326/ghvnc-secret
-git pull origin　main
-git push -u origin main
+#echo +$now >> README.md
+#git add .
+#git commit -m $now
+#git branch -M main
+#git remote add origin https://$Args@github.com/fkunn1326/ghvnc-secret
+#git pull origin　main
+#git push -u origin main
