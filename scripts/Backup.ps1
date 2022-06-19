ce25465e56ef56a0e7631469e8a45a7c4127587c
Fkunn1326 <92153597+fkunn1326@users.noreply.github.com>
2022-06-19 14:58:56 +0900

    Create Backup.ps1

---
+++ scripts/Backup.ps1
@@ -0,0 +1,9 @@
+$now = Get-Date -format "yyyy/MM/dd HH:mm"
+
+Set-Location "C:\Users\runneradmin\AppData\Local\Google\Chrome\User Data"
+git init
+git add .
+git commit -m $now
+git branch -M main
+git remote add origin https://$Args[0]@github.com/fkunn1326/ghvnc-secret
+git push -u origin main
