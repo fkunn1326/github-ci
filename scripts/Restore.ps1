900df8226383e9a4aaece453938f9dd3eabd60cb
Fkunn1326 <92153597+fkunn1326@users.noreply.github.com>
2022-06-20 22:01:59 +0900

    Update Restore.ps1

--- scripts/Restore.ps1
+++ scripts/Restore.ps1
@@ -2,4 +2,4 @@ Set-Location backups
 Expand-Archive backup.zip
 Remove-Item backup.zip
 
-Move-Item　* "C:\Users\runneradmin\AppData\Local\Google\Chrome\User Data"
+Move-Item　.\backup\* "C:\Users\runneradmin\AppData\Local\Google\Chrome\User Data"
