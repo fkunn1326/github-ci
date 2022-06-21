01fc5089cb4925674dd5026bda3b7995406a175e
Fkunn1326 <92153597+fkunn1326@users.noreply.github.com>
2022-06-21 22:22:32 +0900

    Update Backup.ps1

--- scripts/Backup.ps1
+++ scripts/Backup.ps1
@@ -10,11 +10,14 @@ if((get-process "chrome" -ea SilentlyContinue) -eq $Null){
     Stop-Process -processname "chrome"
 }
 
+
+Set-Location "C:\Users\runneradmin\AppData\Local\Google\Chrome\User Data"
 Compress-Archive -Path * -DestinationPath D:\a\github-ci\github-ci\backups\backup.zip -Update
 
-#Set-Location "C:\Users\runneradmin\AppData\Local\Google\Chrome\User Data"
+#Move-Item backups.zip D:\a\github-ci\github-ci\backups
 
 Set-Location D:\a\github-ci\github-ci\backups
+
 Expand-Archive backup.zip
 Remove-Item backup.zip
 
