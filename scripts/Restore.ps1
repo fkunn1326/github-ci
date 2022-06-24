4007dfa6971ae9b5b539c09ee5e684fca004696a
Fkunn1326 <92153597+fkunn1326@users.noreply.github.com>
2022-06-24 19:51:24 +0900

    nnnn?

--- scripts/Restore.ps1
+++ scripts/Restore.ps1
@@ -1,4 +1,4 @@
 Set-Location ./backups/backup
 
-"C:\Program Files\Mozilla Firefox\firefox.exe"
+Start "C:\Program Files\Mozilla Firefox\firefox.exe"
 #Move-Item -Path C:\Users\runneradmin\AppData\Roaming\Mozilla\Firefox\Profiles\*.default
