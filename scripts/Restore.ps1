eecf159ddacb3e697398ca87935249a637af9c3c
Fkunn1326 <92153597+fkunn1326@users.noreply.github.com>
2022-06-24 21:12:03 +0900

    onegai

--- scripts/Restore.ps1
+++ scripts/Restore.ps1
@@ -1,12 +1,11 @@
 Set-Location ./backups/backup
 
-$lpath = Convert-Path .
-
 Start "C:\Program Files\Mozilla Firefox\firefox.exe"
 Start-Sleep -s 20
 Stop-Process -processname "firefox"
 
-$fpath = Convert-Path C:\Users\runneradmin\AppData\Roaming\Mozilla\Firefox\Profiles\*.default-release
-Remove-Item $fpath\*
+$ffpath = Convert-Path C:\Users\runneradmin\AppData\Roaming\Mozilla\Firefox\Profiles\*.default-release
+$fpath = $$ffpath + \*
+Remove-Item $fpath
 
-Move-Item $lpath\* $fpath -Force
+Move-Item D:\a\github-ci\github-ci\backups\backup\* $ffpath -Force
