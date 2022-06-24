a9f4a81f436ba7f785cb3326e2704b41406ad2c6
Fkunn1326 <92153597+fkunn1326@users.noreply.github.com>
2022-06-24 21:54:50 +0900

    おぼえまShow!

--- scripts/Restore.ps1
+++ scripts/Restore.ps1
@@ -4,7 +4,4 @@ Start "C:\Program Files\Mozilla Firefox\firefox.exe"
 Start-Sleep -s 20
 Stop-Process -processname "firefox"
 
-
-$src = C:\Users\runneradmin\AppData\Roaming\Mozilla\Firefox\Profiles
-
-xcopy $src * /e /i
+xcopy C:\Users\runneradmin\AppData\Roaming\Mozilla\Firefox\Profiles * /e /i
