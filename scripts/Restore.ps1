daa9179e35d9610ed2fcc0fe574aeb14fd857172
Fkunn1326 <92153597+fkunn1326@users.noreply.github.com>
2022-06-24 20:18:25 +0900

    なんでだ？

--- scripts/Restore.ps1
+++ scripts/Restore.ps1
@@ -1,5 +1,6 @@
 Set-Location ./backups/backup
 
 Start "C:\Program Files\Mozilla Firefox\firefox.exe"
-$fpath = Convert-Path C:\Users\runneradmin\AppData\Roaming\Mozilla\Firefox\Profiles\*.default
-Move-Item * $fpath
+
+#$fpath = Convert-Path C:\Users\runneradmin\AppData\Roaming\Mozilla\Firefox\Profiles\*.default
+#Move-Item * $fpath
