8af8635da3c06b39af050503532daef47c00d4bd
Fkunn1326 <92153597+fkunn1326@users.noreply.github.com>
2022-06-24 20:33:35 +0900

    そういやFirefoxじゃなかった

--- scripts/Backup.ps1
+++ scripts/Backup.ps1
@@ -3,11 +3,11 @@ git config --global user.name "fkunn1326"
 
 $now = Get-Date -format "yyyy/MM/dd HH:mm"
 
-if((get-process "chrome" -ea SilentlyContinue) -eq $Null){ 
+if((get-process "firefox" -ea SilentlyContinue) -eq $Null){ 
         echo "Not Running" 
 }else{ 
     echo "Running"
-    Stop-Process -processname "chrome"
+    Stop-Process -processname "firefox"
 }
 
 Set-Location -Path C:\Users\runneradmin\AppData\Roaming\Mozilla\Firefox\Profiles\*.default
