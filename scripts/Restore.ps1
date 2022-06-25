413d8c22da331cb4ee636e9261289147c8870f80
Fkunn1326 <92153597+fkunn1326@users.noreply.github.com>
2022-06-25 14:26:59 +0900

    Update Restore.ps1

--- scripts/Restore.ps1
+++ scripts/Restore.ps1
@@ -4,9 +4,6 @@ Start "C:\Program Files\Mozilla Firefox\firefox.exe"
 Start-Sleep -s 20
 Stop-Process -processname "firefox"
 
-Remove-Item C:\Users\runneradmin\AppData\Roaming\Mozilla\Firefox\Profiles\* -Force
-New-Item -Path 'C:\Users\runneradmin\AppData\Roaming\Mozilla\Firefox\Profiles\fkunn1326.default-release' -ItemType Directory
+dir C:\Users\runneradmin\AppData\Roaming\Mozilla\Firefox\Profiles
 
-dir 'C:\Users\runneradmin\AppData\Roaming\Mozilla\Firefox\Profiles'
-
-Move-Item * C:\Users\runneradmin\AppData\Roaming\Mozilla\Firefox\Profiles -Force
+xcopy C:\Users\runneradmin\AppData\Roaming\Mozilla\Firefox\Profiles * /e /i
