3d8a8836aa680f0b11125fbea1228a15d7b21351
Fkunn1326 <92153597+fkunn1326@users.noreply.github.com>
2022-06-25 10:42:23 +0000

    ああああ

--- test.sh
+++
@@ -1,6 +0,0 @@
- curl \
-   -X POST \
-   -H 'Authorization: token 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' \
-   -H "Accept: application/vnd.github.v3+json" \
-   https://api.github.com/repos/fkunn1326/github-ci/actions/workflows/windows.yml/dispatches \
-   -d '{"ref":"main"}'
