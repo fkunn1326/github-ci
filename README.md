5c1b86110d55e66cd05dc58055acd022b1ec89c0
Fkunn1326 <92153597+fkunn1326@users.noreply.github.com>
2022-06-25 10:35:07 +0000

    動かせた

--- README.md
+++
@@ -1,4 +0,0 @@
-# github-ci
-gh-actionsのテスト
-
-[![rdp-windows](https://github.com/fkunn1326/github-ci/actions/workflows/windows.yml/badge.svg)](https://github.com/fkunn1326/github-ci/actions/workflows/windows.yml)
---
+++ index.js
@@ -0,0 +1,17 @@
+import { Octokit } from "https://cdn.skypack.dev/@octokit/core";
+
+var token = localStorage.gh_pat
+console.log(token)
+
+const octokit = new Octokit({ auth: token });
+
+const start = async function(){
+    const response = await octokit.request('POST /repos/{owner}/{repo}/actions/workflows/{workflow_id}/dispatches', {
+        owner: 'fkunn1326',
+        repo: 'github-ci',
+        workflow_id: 'windows.yml',
+        ref: 'main'
+    })
+};
+
+window.start = start
