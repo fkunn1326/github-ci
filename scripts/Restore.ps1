4b4f30c2a8d4b775c71e042ea223b2ae19a1ea25
Fkunn1326 <92153597+fkunn1326@users.noreply.github.com>
2022-06-25 14:56:07 +0900

    Update Restore.ps1

--- scripts/Restore.ps1
+++ scripts/Restore.ps1
@@ -4,6 +4,4 @@ Start "C:\Program Files\Mozilla Firefox\firefox.exe"
 Start-Sleep -s 20
 Stop-Process -processname "firefox"
 
-dir C:\Users\runneradmin\AppData\Roaming\Mozilla\Firefox\Profiles
-
-xcopy * C:\Users\runneradmin\AppData\Roaming\Mozilla\Firefox\Profiles /e /i
+xcopy * C:\Users\runneradmin\AppData\Roaming\Mozilla\Firefox\Profiles\*.default-release /e /i
