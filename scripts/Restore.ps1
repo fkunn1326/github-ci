503f792016628fac10f6615c376674919e7fc6fb
Fkunn1326 <92153597+fkunn1326@users.noreply.github.com>
2022-06-30 18:38:54 +0900

    Update Restore.ps1

--- scripts/Restore.ps1
+++ scripts/Restore.ps1
@@ -5,6 +5,6 @@ Start-Sleep -s 20
 
 Stop-Process -processname "firefox"
 
-$str_path = (Convert-Path C:\Users\runneradmin\AppData\Roaming\Mozilla\Firefox\Profiles\*.default-release)
+#$str_path = (Convert-Path C:\Users\runneradmin\AppData\Roaming\Mozilla\Firefox\Profiles\*.default-release)
 
 xcopy * $str_path  /e /i /r /y /c
