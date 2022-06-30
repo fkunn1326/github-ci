0aaedc60b5252ed9965472ce4412acbf9df4c26c
Fkunn1326 <92153597+fkunn1326@users.noreply.github.com>
2022-06-30 18:34:12 +0900

    Update Restore.ps1

--- scripts/Restore.ps1
+++ scripts/Restore.ps1
@@ -2,9 +2,8 @@ Set-Location ./backups/backup
 
 Start "C:\Program Files\Mozilla Firefox\firefox.exe"
 Start-Sleep -s 20
-Stop-Process -processname "firefox"
 
-dir C:\Users\runneradmin\AppData\Roaming\Mozilla\Firefox\Profiles\*.default-release
+Stop-Process -processname "firefox"
 
 $str_path = (Convert-Path C:\Users\runneradmin\AppData\Roaming\Mozilla\Firefox\Profiles\*.default-release)
 
