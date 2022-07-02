8adb5c83cdc569e8f153eb5bf4ba51c2cb64ae65
Fkunn1326 <92153597+fkunn1326@users.noreply.github.com>
2022-07-02 19:18:59 +0900

    Update Restore.ps1

--- scripts/Restore.ps1
+++ scripts/Restore.ps1
@@ -4,7 +4,7 @@ Start "C:\Program Files\Mozilla Firefox\firefox.exe"
 Start-Sleep -s 20
 
 Stop-Process -processname "firefox"
-Wait-Process -processname "firefox"
+Start-Sleep -s 20
 
 dir C:\Users\runneradmin\AppData\Roaming\Mozilla\Firefox\Profiles\
 
