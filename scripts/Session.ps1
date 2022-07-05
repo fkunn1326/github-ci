1d16f57d28987d42828fe1d79112ebec3046891a
Fkunn1326 <92153597+fkunn1326@users.noreply.github.com>
2022-07-06 07:28:16 +0900

    Update Session.ps1

--- scripts/Session.ps1
+++ scripts/Session.ps1
@@ -1,6 +1,6 @@
 while ($true){
   "C:\Program Files\Mozilla Firefox\firefox.exe -url $Args"
-  Start-Sleep -s 20
+  Start-Sleep -s 50
   
   Stop-Process -processname "firefox"
   Start-Sleep -s 1800
