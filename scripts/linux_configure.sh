a48ad8c254213c0f17e720336c00702be34f8cdf
Fkunn1326 <92153597+fkunn1326@users.noreply.github.com>
2022-07-23 09:57:57 +0900

    Create linux

--- scripts/linux_configure.sh
+++ scripts/linux_configure.sh
@@ -1,10 +1,15 @@
 # First, install required packages...
 sudo apt update
-sudo apt install -y socat xfce4 xfce4-goodies xfonts-base xubuntu-icon-theme xubuntu-wallpapers gnome-icon-theme x11-apps x11-common x11-session-utils x11-utils x11-xserver-utils x11-xkb-utils dbus-user-session dbus-x11 gnome-system-monitor gnome-control-center libpam0g libxt6 libxext6
+sudo apt install -y xfce4 xfce4-goodies xfonts-base xubuntu-icon-theme xubuntu-wallpapers gnome-icon-theme x11-apps x11-common x11-session-utils x11-utils x11-xserver-utils x11-xkb-utils dbus-user-session dbus-x11 gnome-system-monitor gnome-control-center libpam0g libxt6 libxext6
 
 # Second, install TurboVNC
-wget https://jaist.dl.sourceforge.net/project/virtualgl/3.0.1/virtualgl_3.0.1_amd64.deb
+wget https://phoenixnap.dl.sourceforge.net/project/turbovnc/2.2.5/turbovnc_2.2.5_amd64.deb
 sudo dpkg -i turbovnc_2.2.5_amd64.deb
+
+# Third install Piping
+wget https://github.com/nwtgck/go-piping-tunnel/releases/download/v0.10.2/piping-tunnel-0.10.2-linux-amd64.deb
+sudo dpkg -i piping-tunnel-0.10.2-linux-amd64.deb 
+
 
 # Fourth, generate and copy passwd file and xstartup script
 export PATH=$PATH:/opt/TurboVNC/bin
@@ -14,4 +19,5 @@ echo $VNC_USER_PASSWORD | vncpasswd -f > $HOME/.vnc/passwd
 chmod 0600 $HOME/.vnc/passwd
 
 # Fifth and last, set up auth token from argument
+
 exit
